a8b93e22d4da7f5cd2e80189a156ec95279e6b8a
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2023-05-23 14:47:55 +0100

    set up pages components with simple remdering for project setup

---
+++ src/pages/Projects.js
@@ -0,0 +1,10 @@
+const Project = () => {
+    return (
+        <div>
+            <h1>Projects</h1>
+            <p>This is the project page</p>
+        </div>
+    )
+}
+
+export default Project;
